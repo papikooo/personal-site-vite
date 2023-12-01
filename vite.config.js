11845b37773b0f3223511bd1c025ca99405cddec
nmori <harupapiko@gmail.com>
2023-12-01 19:02:00 +0900

    rollupでエラー中

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import glob from 'fast-glob';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   root: 'src',
   build: {
     base: './', //相対パスでビルド
     outDir: '../dist', //出力場所の指定
+    rollupOptions: {
+      // ライブラリにバンドルされるべきではない依存関係（vue）を外部化
+      external: ['vue'],
+      // scssのビルド - scss/以下の.scssをビルドする（_から始まるファイルは除く）
+      input: glob.sync(['src/scss/**/*.scss', '!**/_*']), 
+      output: {
+        assetFileNames: ({name}) => `${name?.replace('scss/', 'css/')}`,
+         // 外部化された依存関係のために UMD のビルドで使用するグローバル変数を提供
+        globals: {
+          vue: 'Vue',
+        },
+      }
+    }
   }
 })
