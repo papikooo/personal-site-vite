237c6c579aae910a9be5ab935d625ad12a652158
nmori <harupapiko@gmail.com>
2023-12-07 11:54:04 +0900

    build時のディレクトリ調整（一旦OK）

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import glob from 'fast-glob';
+import globule from 'globule';
+// import glob from 'fast-glob';
+
+const htmlFiles = globule.find('src/**/*.html');
+// 無視したいファイルがあるとき第二引数に記載
+// {
+//   ignore : [
+//     'src/assets/_*.js'
+//   ]
+// }
 
 export default defineConfig({
   plugins: [vue()],
@@ -8,18 +17,42 @@ export default defineConfig({
   build: {
     base: './', //相対パスでビルド
     outDir: '../dist', //出力場所の指定
+    emptyOutDir: true,
     rollupOptions: {
       // ライブラリにバンドルされるべきではない依存関係（vue）を外部化
       external: ['vue'],
-      // 外部化された依存関係のために UMD のビルドで使用するグローバル変数を提供
+      // 外部化された依存関係のために UMD（moduleの一種） のビルドで使用するグローバル変数を提供
       globals: {
       vue: 'Vue',
       },
-      // scssのビルド - scss/以下の.scssをビルドする（_から始まるファイルは除く）
-      input: glob.sync(['src/assets/scss/**/*.scss', '!**/_*']), 
+      input: htmlFiles,
       output: {
-        assetFileNames: ({name}) => `${name?.replace('scss/', 'css/')}`,
+        // build時の名前をentry、chunk、assets毎に指定（既定のハッシュを消す）
+        entryFileNames: `assets/js/[name].js`,
+        chunkFileNames: `assets/js/[name].js`,
+        assetFileNames: (assetInfo) => {
+          const {name} = assetInfo
+          // if分で無理矢理ディレクトリ分け・・・
+          if (/\.(jpe?g|png|gif|svg)$/.test(name ?? '')) {
+            return 'assets/images/[name][ext]';
+          }
+          if (/\.(css)$/.test(name ?? '')) {
+            return 'assets/css/[name].css';
+          }
+          if (/\.(js)$/.test(name ?? '')) {
+            return 'assets/js/[name].js';
+          }
+          return 'assets/[name].[ext]';
+        }
       }
+      
+      // scssのビルド - scss/以下の.scssをビルドする（_から始まるファイルは除く）
+      // [
+      //   glob.sync(['src/assets/scss/**/*.scss', '!**/_*']),
+      // ],
+      // output: {
+      //   assetFileNames: ({name}) => `${name?.replace('scss/', 'css/')}`,
+      // }
     }
   }
 })
