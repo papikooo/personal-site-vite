9305febbdfefd6425d0cbe8804f204eb7f04f193
nmori <harupapiko@gmail.com>
2023-12-29 11:11:05 +0900

    top調整

--- src/components/pages/Top.vue
+++ src/components/pages/Top.vue
@@ -5,6 +5,9 @@ defineProps({
 </script>
 
 <template>
+	<div class="top_fv">
+    <img src="/assets/images/common/main_dummy.png" alt="">
+	</div>
 	<h2>これはトップページです</h2>
 	<p>Hello World!</p>
 </template>
