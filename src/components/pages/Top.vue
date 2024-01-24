ff7246c87e31c258ed63249cf6432eaa0ab4b7d3
nmori <harupapiko@gmail.com>
2024-01-24 18:28:35 +0900

    240124職場

--- src/components/pages/Top.vue
+++ src/components/pages/Top.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Button from '@/components/Button.vue'
+import Card from '@/components/Card.vue'
 
 // defineProps({
 // 	title: String,
@@ -11,11 +12,20 @@ import Button from '@/components/Button.vue'
 	<div class="top_fv">
     <img src="/assets/images/common/fv_dummy.jpg" alt="">
 	</div>
-	<h2>Notes</h2>
-	<p>Hello World!</p>
-	<router-link to="/notes"><Button button_text="and&nbsp;more..." /></router-link>
-	<h2>Works</h2>
-	<router-link to="/works"><Button button_text="and&nbsp;more..." /></router-link>
-	<h2>Plan</h2>
-	<router-link to="/plan"><Button button_text="and&nbsp;more..." /></router-link>
+	<div class="inner">
+		<h2>Notes</h2>
+		<p>Hello World!</p>
+		<div class="card_area">
+			<Card card_text="テスト"/>
+		</div>
+		<router-link to="/notes"><Button button_text="and&nbsp;more..." /></router-link>
+	</div>
+	<div class="inner">
+		<h2>Works</h2>
+		<router-link to="/works"><Button button_text="and&nbsp;more..." /></router-link>
+	</div>
+	<div class="inner">
+		<h2>Plan</h2>
+		<router-link to="/plan"><Button button_text="and&nbsp;more..." /></router-link>
+	</div>
 </template>
