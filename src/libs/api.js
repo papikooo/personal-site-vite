a25056a71ea360c241615075fb0e164b2a4aa1bb
nmori <harupapiko@gmail.com>
2025-03-25 18:09:33 +0900

    api urlをvercelに

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,11 +1,11 @@
 // api.js
 export const fetchBlogs = async (params) => {
   try {
-    let url = '/api/notes'; // 基本のURL
+    let url = 'https://personal-site-versel.vercel.app/api/notes'; // 基本のURL
 
     // contentIdが指定されている場合は、特定の記事を取得するAPIに変更
     if (params.contentId) {
-      url = `http://localhost:3000/api/notes/${params.contentId}`; // サーバーの API 経由でデータを取得
+      url = `https://personal-site-versel.vercel.app/api/notes/${params.contentId}`; // サーバーの API 経由でデータを取得
     }
     // category が指定されている場合、クエリパラメータを追加
     else if (params.category) {
